334f5c6e3b8fb7b52302b8cb54e82f6d44caa1fb
clavoie <clavoie@gmail.com>
2018-07-28 13:54:58 -0400

    Minor refactoring. Cleaning up some comments, was going to move struct Def behind an interface but thought better of it

--- def.go
+++ def.go
@@ -1,37 +1,28 @@
 package di
 
 // Def represents a dependency definition
-type IDef interface {
-	// Constructor returns the constructor value for this IDef
-	Constructor() interface{}
-
-	// Lifetime returns the lifetime value of this IDef
-	Lifetime() Lifetime
-}
-
-// Def represents a dependency definition
 type Def struct {
-	// constructor must be a func of the signature:
-	//		func Name(dependency*) (Interface, error?)
-	// Examples:
-	//    func Foo1() Dependency
-	//    func Foo2(dep1 Dep1) Dependency
-	//    func Foo3(dep1, dep2 Dep1) (Dependency, error)
+	// constructor is a func which instantiates the dependency
 	constructor interface{}
 
-	// lifetime is the caching lifetime of the dependency once it has been
-	// resolved
+	// lifetime is the caching lifetime of the dependency
 	lifetime Lifetime
 }
 
-// NewDef creates a new dependency definition which can be added to a Defs collection. See Def.Constructor for
-// the format of the constructor parameter
+// NewDef creates a new dependency definition which can be added to a Defs collection.
+//
+// constructor must be a func of the signature:
+//		func Name(dependency*) (Interface, error?)
+// Examples:
+//    func Foo1() Dependency
+//    func Foo2(dep1 Dep1) Dependency
+//    func Foo3(dep1, dep2 Dep1) (Dependency, error)
+//
+// lifetime is the caching lifetime of the dependency once it has been
+// resolved
 func NewDef(constructor interface{}, lifetime Lifetime) *Def {
 	return &Def{
 		constructor: constructor,
 		lifetime:    lifetime,
 	}
 }
-
-func (d *Def) Constructor() interface{} { return d.constructor }
-func (d *Def) Lifetime() Lifetime       { return d.lifetime }
--- err_resolve.go
+++ err_resolve.go
@@ -14,11 +14,13 @@ type ErrResolve struct {
 	// DependencyChain is the chain of types leading up to the
 	// type that could not be resolved. Does not contain Type
 	DependencyChain []reflect.Type
+
 	// Err represents the error that caused the resolution error. If
 	// a definition was missing this will be an *ErrDefMissing,
 	// otherwise it will be whatever error was returned by the
 	// dependency constructor
 	Err error
+
 	// Type is the type of dependency which could not be resolved
 	Type reflect.Type
 }
