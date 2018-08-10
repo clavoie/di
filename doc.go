8db5b4ba98d3358662ef5b4d8b94d585fe8bdb22
Chris LaVoie <clavoie@gmail.com>
2018-08-09 22:31:11 -0400

    touch

--- doc.go
+++ doc.go
@@ -3,6 +3,7 @@
 di supplies several dependency lifetime caching policies, provides dependency aware http handlers compatible with net/http,
 and provides a way to clean up dependencies instantiated during an http request.
 
+
 di only resolves dependencies which are interfaces, the resolver itself, http.ResponseWriter, and *http.Request.
 */
 package di
